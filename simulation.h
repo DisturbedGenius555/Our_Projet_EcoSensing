0c3f83fb9bced7df07fb501280438ada24203bc6
mark-olf <fleury.kassa1@gmail.com>
2026-01-11 22:22:59 +0100

    Add simulation.h for transmission functions

---
+++ simulation.h
@@ -0,0 +1,8 @@
+#ifndef SIMULATION_H_INCLUDED
+#define SIMULATION_H_INCLUDED
+
+#include "structures.h"
+
+int attempt_transmission(Capteur *c);
+float calculer_energie_transmission(float distance);
+#endif // SIMULATION_H_INCLUDED
